745b961d5ab284fca4c42d4e3319d66f16a4c44a
KURURU <i@okako.com>
2018-07-31 12:53:40 +0900

    1

--- main-normal.c
+++ main-normal.c
@@ -43,111 +43,18 @@ static void Delay_ms(unsigned int DELAY_CNT) {
     }
 }
 
-static int abs(int t) {
-    if(t < 0 ) return -t;
-    if(t >= 0) return t;
-}
-
-unsigned int adconv(void)
-{
-     unsigned int temp;
-
-     GO_nDONE = 1 ;         // PICにアナログ値読取り開始を指示
-     while(GO_nDONE) ;      // PICが読取り完了するまで待つ
-     temp = ADRESH ;        // PICは読取った値をADRESHとADRESLのレジスターにセットする
-     temp = ( temp << 8 ) | ADRESL ;  // 10ビットの分解能力です
-
-     return temp ;
-}
-
 void main(void) {
     
-    unsigned int temp;
-    unsigned int addat;
-    int width = 0;
-    int max_width = 0;
-    long int panicTimer = 0;
     int i = 0;
 
- //   OSCCON  = 0b01110000; // 内部クロックを32MHzとする
- //   OSCCON  = 0b01111010; // 内部クロックを16MHzとする
     OSCCON = 0b01110010; // 内部クロックは8MHzとする PLLEN = ONにて4倍速動作指定済み
-    ANSELA = 0b0000001; // アナログはAN0を使用し、残りをすべてデジタルI/Oに割当
-    TRISA  = 0b00001001; // RA3は入力専用, アナログはAN0を使用
-    ADCON0 = 0b00000001; // アナログ変換情報設定(RA0ポートのAN0から読込む)
-    __delay_us(5);
-    ADCON1 = 0b10010000; // 読取値は右寄せ、A/D変換クロックはFOSC/8、VDDをリファレンスとする
-//    ADCON1 = 0b10100000; // 読取値は右寄せ、A/D変換クロックはFOSC/32、VDDをリファレンスとする
-    Delay_ms(1); // アナログ変換情報が設定されるまでとりあえず待つ (*1:20→5)
+    ANSELA = 0b0000000; // アナログ入力は停止、すべてデジタルI/Oに割当
+    TRISA  = 0b0000110; // RA2は入力、RA3は入力専用
 
-    RA2 = 0; RA1 = 0;
+    RA1 = 0; RA0 = 0;
     RA5 = 0; RA4 = 0;
-    
-    Delay_ms(100);
-    
-    /*
-    while(1) {
-        if((width > 0)&&(RA3 == 0)) {
-            max_width = width;
-            width = 0;
-            GO = 1; // start conversion
-            while(nDONE);  // wait conversion
-            addat = ADRESH ;        // PICは読取った値をADRESHとADRESLのレジスターにセットする
-            addat = ( addat << 8 ) | ADRESL ;  // 10ビットの分解能力です
-        };
-   
-        if((width == 0)&&(RA3 == 1)) {
-            while(RA3) width++;
-        };
- 
-        if(max_width <  240) {RA1=1; RA2=0;}; 
-        if((240 <=  max_width)&&(max_width <  600)) {RA1=1; RA2=1;};
-        if(600 <= max_width) {RA1=0; RA2=1;};           
-
-
-        if(addat <  20) {RA5=1; RA4=0;}; 
-        if((20 <=  addat)&&(addat <  1003)) {RA5=1; RA4=1;};
-        if(1003 <= addat) {RA5=0; RA4=1;};           
-    };
-    */
-    
-    while(1) {
         
-        if((width > 0)&&(RA3 == 0)) {
- //           RA2 = 1; RA1 = 1; // Lock
- //           RA5 = 1; RA4 = 1; // Lock
-            max_width = (width - 250)*3;// 0
-            if(max_width<0) max_width = 0;
-            width = 0;
-            GO = 1; // start conversion
-            while(nDONE);  // wait conversion
-            addat = ADRESH ;        // PICは読取った値をADRESHとADRESLのレジスターにセットする
-            addat = ( addat << 8 ) | ADRESL ;  // 10ビットの分解能力です
-        };
-        
-        if((width == 0)&&(RA3 == 1)) {
-            while(RA3) width++;
-        };
-                   
-        if((5 <= max_width)&&(max_width < 973)) { // 
-            
-            if(abs(addat - max_width) > 60) {//30
-                panicTimer = 80000;// 80000
-            };
-           
-            panicTimer--;if (panicTimer < 0) panicTimer = 0;
-            
-            if((abs(addat - max_width) > 30)&&(panicTimer > 0)) {
-                
-                if(addat > max_width) {
-                    RA5 = 0; RA4 = 1;
-                } else {
-                    RA5 = 1; RA4 = 0;
-                };
-                
-            } else {RA5 = 1; RA4 = 1;}; // lock // if(abs(ADRES - max_width) > 25)
-
-        } else {RA5 = 0; RA4 = 0;}; // free // if((5 <= max_width)&&(max_width < 973))
+    while(1) {
         
     };  //  while(1)
 
